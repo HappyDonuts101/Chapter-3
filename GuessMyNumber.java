fe32d0e8fd698ce0414ee227fbc16d88977975e9
HappyDonuts101 <ethan.ping.song@gmail.com>
2026-09-17 14:29:15 -0700

    Update GuessMyNumber.java

--- GuessMyNumber.java
+++ GuessMyNumber.java
@@ -1,7 +1,7 @@
 import java.util.Random;
 import java.util.Scanner;
 
-public class guessNumber {
+public class GuessMyNumber {
 
     public static void main(String[] args) {
         // pick a random number
