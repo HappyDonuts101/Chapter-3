b6f851da5e0d16776e0d6372ae5b4d947b497448
HappyDonuts101 <ethan.ping.song@gmail.com>
2026-09-17 14:26:53 -0700

    Add files via upload

---
+++ convertTime.java
@@ -0,0 +1,17 @@
+import java.util.Scanner; 
+
+public class convertTime {
+
+	public static void main(String[] args) {
+
+		int time, hour, seconds, minutes;
+		Scanner in = new Scanner(System.in);
+		System.out.print("Please enter an integer: " );
+		time = in.nextInt();
+		hour = time/3600;
+		minutes = (time%3600)/60;
+		seconds = (time%3600)%60;
+		System.out.printf("%d seconds = %d hours, %d minutes, and %d seconds", time, hour ,minutes, seconds);
+    }
+
+}
---
+++ guessNumber.java
@@ -0,0 +1,23 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class guessNumber {
+
+    public static void main(String[] args) {
+        // pick a random number
+        int userNumber;
+        int difference;
+        Random random = new Random();
+        int number = random.nextInt(100) + 1;
+        Scanner in = new Scanner(System.in);
+        System.out.println("I'm thinking of a number between 1 and 100 (including both). Can you guess what it is?" );
+        System.out.print("Type a number: " );
+        userNumber = in.nextInt();
+        System.out.println("Your guess is: " + userNumber);
+        System.out.println("The number I was thinking of is: " + number);
+        difference = Math.abs(userNumber-number);
+        System.out.print("You were off by " + difference);
+                
+        
+    }
+}
