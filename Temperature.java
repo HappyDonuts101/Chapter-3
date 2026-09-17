0ce2a8ed08f4022bf197ec63b13ee86982a94dee
HappyDonuts101 <ethan.ping.song@gmail.com>
2026-09-16 18:13:24 -0700

    Update Temperature.java

--- Temperature.java
+++ Temperature.java
@@ -1,21 +1,18 @@
 import java.util.Scanner;
 
-public class Temperature {
+public class Temperature{
 
-public static void main (String[] args) {
+    public static void main (String[] args) {
 	
-	double celsius;
-	double Farenheit;
-	
-	Scanner in = new Scanner(System.in);
-	System.out.print("Enter a temperature in Celsius:" );
-	celsius = in.nextDouble();
-	
-	Farenheit = celsius * 9/5 + 32;
-	System.out.printf("The temperature in farenheit is = %.1f" , Farenheit);
-
-	}
-
-
-
+        double celsius;
+        double farenheit;
+        
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter a temperature in Celsius:" );
+        celsius = in.nextDouble();
+        
+        farenheit = celsius * 9/5 + 32;
+        System.out.printf("%.1f C = %.1f F", celsius, farenheit);  
+    }
 }
+
